e45f3286214f9b1b1b8438fb940d7cf3acef3e83
emiliaskiba <84234350+emiliaskiba@users.noreply.github.com>
2021-12-02 23:58:33 +0100

    Add files via upload

---
+++ files/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>EMILIA SKIBA</title>
+<meta name="title" content="example" />
+</head>
+<body>
+EMILIA SKIBA
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>EMILIA SKIBA</title>
+<meta name="title" content="example" />
+<link rel="stylesheet" href="www.example.com/css/css.css" type="text/css" />
+</head>
+<body>
+<h1>Działa</h1>
+</body>
+</html>
+
